b84c287e45f59cead9057999f5d9dbbd1ac5dfda
SEclipseDS <riley8seo@gmail.com>
2023-06-02 00:57:08 +0900

    Edit format

--- scripts/ref.py
+++ scripts/ref.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pandas as pd
 import os
 import re
@@ -47,3 +48,53 @@ refdf["fa"] = fasta
 refdf["annot"] = annotation
 
 refdf.to_csv(f"{DATAPATH}/config/genome.csv", index=False)
+=======
+import glob
+import os
+
+import pandas as pd
+
+DATAPATH = os.getcwd()
+
+filecnt = len(list(os.listdir("./genome/")))
+
+fafiles = ("./genome/*.fa", "./genome/*.fasta")  # the tuple of file types
+fasta = []
+for files in fafiles:
+    fasta.extend(glob.glob(files))
+
+names = os.path.splitext(fasta[0])[0].split("/")[-1]
+
+afiles = (
+    "./genome/*.gtf",
+    "./genome/*.gff3",
+    "./genome/*.gff",
+)  # the tuple of file types
+
+annotation = []
+for files in afiles:
+    annotation.extend(glob.glob(files))
+
+a = annotation[0]
+
+if a.endswith("gtf"):
+    os.rename(a, f"./genome/{names}.gtf")
+elif a.endswith("gff3"):
+    os.rename(a, f"./genome/{names}.gff3")
+elif a.endswith("gff"):
+    os.rename(a, f"./genome/{names}.gff")
+
+new = ("./genome/*.gtf", "./genome/*.gff3", "./genome/*.gff")  # the tuple of file types
+
+annotation = []
+for files in new:
+    annotation.extend(glob.glob(files))
+
+refdf = pd.DataFrame({"name": [], "fa": [], "annot": []})
+
+refdf["name"] = [names]
+refdf["fa"] = fasta
+refdf["annot"] = annotation
+
+refdf.to_csv(f"{DATAPATH}/config/genome.csv", index=False)
+>>>>>>> a879797 (Edit format)
